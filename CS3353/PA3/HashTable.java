9c4e370d3a9a36bf10c267f6955034c3681ebd81
Parker Hague <parkerhague@gmail.com>
2020-03-22 16:07:24 -0500

    Finished HashTable Chaining

--- CS3353/PA3/HashTable.java
+++ CS3353/PA3/HashTable.java
@@ -1,12 +1,26 @@
+
+/**
+ * 
+ * 
+ * Parker Hague
+ * 
+ * CS3353
+ * Programming Assignment 3
+ * 
+ * 
+ * HashTable with chaining
+ */
+
+
+
+import java.io.*;
+
 public class HashTable{
-
-    
-
-
 
     private HashNode[] table;
     private int size;
     private int tableSize;
+
 
 
     public HashTable(int tableSize){
@@ -16,9 +30,11 @@ public class HashTable{
     }
 
 
-
-
     public void insert(int toInsert){
+
+        if (size >= .6 * table.length){
+            //resize();
+        }
 
         int index = hashFunc(toInsert);
 
@@ -31,48 +47,132 @@ public class HashTable{
             newEntry.next = table[index];
             table[index] = newEntry;
         }
+        size++;
     }
 
-    public void delete(int numData) {
-		int loc = hashFunc(numData);
-		HashNode start = table[loc];
-		HashNode end = start;
-		//System.out.println(size);
 
+    public void delete(int toDelete){
 
-		if (search(numData)) {
-			if (start.data == numData) {
-				size--;
-				table[loc] = start.next;
-				return;
-			}
+        int index = hashFunc(toDelete);
+    
+        HashNode first = table[index];
+        HashNode temp = first;
 
-			while (end.next != null && end.next.data != numData) {
-				end = end.next;
-				if (end.next == null) {
-					//System.out.println("\n" + numData + "not found\n");
-					return;
-				}
-				size--;
-				if (end.next.next == null) {
-					end.next = null;
-					return;
-				}
-				end.next = end.next.next;
-				table[loc] = start;
-			}
-		}
-	}
+        if (first == null){
+            return;
+        }
+    
+        if (first.data == toDelete){ // if removal is first in index
+    
+            table[index] = first.next;
+            size--;
+            return;
+        }
+
+        if (first.next.data == toDelete){ // if removal is second in index
+
+            first.next = null;
+            size--;
+            return;
+        }
+    
+        while(temp.next.data != toDelete){
+    
+            temp = temp.next;
+    
+            if (temp.next == null){
+                return;
+            }
+    
+            size--;
+    
+            if (temp.next.next == null){ // means it's the last item in the index
+                temp.next = null;
+                return;
+            }
+         
+            table[index] = first;
+            temp.next = temp.next.next;
+        }
+        temp.next = null;
+    }
 
     
-
-
     public int hashFunc(Object toHash){
 
         int hash = toHash.hashCode();
         int index = hash % table.length;
         return index;
     }
+
+
+    public void resize(){
+
+        int primeSize = prime();
+
+        HashNode[] newTable = new HashNode[primeSize]; // creates new table with double the length
+
+        for (int i = 0; i < table.length; i++){
+
+            HashNode head = table[i]; // going to go through each element in index
+
+
+            if (head == null){ // no elements in index
+                continue;
+            }
+         
+
+            if (head != null && head.next == null){ // if only one element in index
+
+                int index = head.hashCode() % newTable.length;
+
+                if (newTable[index] == null){
+                    newTable[index] = head;
+                }
+                else{
+                    head.next = newTable[index];
+                    newTable[index] = head;
+                }
+                continue;
+            }
+  
+
+            while (head != null){ // more than two elements in index
+
+                HashNode temp = head.next;
+
+                int index = temp.hashCode() % newTable.length;
+
+                newTable[index] = temp;
+
+                head.next = newTable[index];
+                newTable[index] = head;
+                head = temp;
+            }
+        }
+        table = newTable;
+    }
+
+
+    public int prime(){
+
+		for (int i = table.length - 1; i >= 1; i++){
+
+            int temp = 0;
+            
+			for (int j = 2; j <= (int) Math.sqrt(i); j++){
+
+				if (i % j == 0){
+					temp++;
+                }
+                
+				if (temp == 0){
+					return i;
+				}
+			}
+		}
+		return 3;
+	}
 
 
     public void printHashTable(){
@@ -92,6 +192,7 @@ public class HashTable{
         System.out.println();
     }
 
+
     public boolean search(int toSearch){
 
         int index = hashFunc(toSearch);
@@ -109,32 +210,60 @@ public class HashTable{
 
 
     public static void main(String[] args) {
+
+        HashTable test;
+        String inFileName = args[0]; // file pathname
+        File inFile = new File(inFileName);
+
+        String instructions;
         
-        HashTable test = new HashTable(13);
+        String[] commands;
+        String[] dotCommands;
+      
+        try {
 
+            BufferedReader in = new BufferedReader(new FileReader(inFile));
 
-        
-        test.insert(1);
-        test.insert(2);
-        test.insert(15);
-        test.insert(5);
-        test.insert(7);
-        test.insert(11);
-        test.insert(12);
+            while ((instructions = in.readLine()) != null){
 
+                commands = instructions.split(" ");
 
-        test.printHashTable();
+                int tableSize = Integer.parseInt(commands[0]);
+                test = new HashTable(tableSize);
 
+                for (String command : commands) {
+                
+                
+                    if (command == commands[0]) continue;
 
-        System.out.println(test.search(2));
+                    if (command.contains(".")){
 
-        test.delete(2);
+                        dotCommands = command.split("\\.");
 
-        System.out.println(test.search(2));
+                        int operand = Integer.parseInt(dotCommands[0]);
+                        String operator = dotCommands[1];
 
-        test.printHashTable();
+                        if (operator.equals("in")){
 
-        
+                            test.insert(operand);
+                        }
+                        
+                        if (operator.equals("del")){
 
+                            test.delete(operand);
+                        }
+                    }
+                }
+
+                test.printHashTable();
+            }
+
+        } catch (FileNotFoundException e) {
+            
+            System.out.println("FILE NOT FOUND");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
