e2d74bb0aea700c782dda67632216719a0797d20
ParkerH98 <61128754+ParkerH98@users.noreply.github.com>
2020-05-10 03:13:05 -0500

    Add files via upload

---
+++ MSIS2203/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            if (254 < 255)
+
+            {   //this is going to print "Hello World!"
+                //I also added an if statement and a couple of extra lines
+                // of text just for fun and practice.
+
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("My name is Parker Hague.");
+                Console.WriteLine("Nice to meet you!");
+            }
+        }
+    }
+}
+
