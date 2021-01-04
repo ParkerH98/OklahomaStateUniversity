d7c871458cb09aba45113cfe44bf84dbabf14aa2
Parker Hague <61128754+ParkerH98@users.noreply.github.com>
2021-01-03 21:36:45 -0600

    Add files via upload

---
+++ MovieRatings/ratings.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# test
+
+
+movieFields = ['id', 'title', 'vote_average']
+castFields = ['id', 'cast']
+
+
+movieData = pd.read_csv('archive/movies_metadata.csv', skipinitialspace=True, usecols=movieFields)
+
+castData = pd.read_csv('archive/credits.csv', skipinitialspace=True, usecols=castFields)
+
+# df = df[df.employee_name != 'chad']
+dfMovies = pd.DataFrame(movieData)
+dfMovies = dfMovies[dfMovies['title'] != 'Nan'] 
+dfCast = pd.DataFrame(castData)
+
+df = pd.merge(dfMovies, dfCast, how='inner')
+
+# cast = dfTemp['cast']
+
+
+# print(df.sort_values(by='id'))
