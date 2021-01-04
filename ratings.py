5f3838c4e5e7df9ff7783cd9ff661329188547cc
Parker Hague <parkerhague@gmail.com>
2021-01-04 02:55:16 -0600

    test

--- ratings.py
+++ ratings.py
@@ -1,26 +1,43 @@
 import numpy as np
+import re
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression  
 
 # test
+# test
+# test
+# test
+
+
+
 
 
 movieFields = ['id', 'title', 'vote_average']
 castFields = ['id', 'cast']
 
+movieData = pd.read_csv('../archive/movies_metadata.csv', skipinitialspace=True, usecols=movieFields)
+castData = pd.read_csv('../archive/credits.csv', skipinitialspace=True, usecols=castFields)
 
-movieData = pd.read_csv('archive/movies_metadata.csv', skipinitialspace=True, usecols=movieFields)
-
-castData = pd.read_csv('archive/credits.csv', skipinitialspace=True, usecols=castFields)
-
-# df = df[df.employee_name != 'chad']
 dfMovies = pd.DataFrame(movieData)
-dfMovies = dfMovies[dfMovies['title'] != 'Nan'] 
+dfMovies['title'] = dfMovies['title'].astype(str)
+dfMovies['id'] = dfMovies['id'].astype(int)
+
 dfCast = pd.DataFrame(castData)
+dfCast['cast'] = dfCast['cast'].astype(str)
+dfCast['id'] = dfCast['id'].astype(int)
 
 df = pd.merge(dfMovies, dfCast, how='inner')
 
-# cast = dfTemp['cast']
+names = df['cast']
+
+actors = []
+
+for row in names:
+
+    line = [re.findall("(?<=\'name\': )(.*?)(?=,)", row)]
+    actors.append(line)
 
 
-# print(df.sort_values(by='id'))
+df['cast'] = actors
+
+print(df)
