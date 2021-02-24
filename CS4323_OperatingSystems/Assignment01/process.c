eb3eca71b6c6dee709a3baf46ec2932edcc4cb2b
ParkerH98 <parkerhague@gmail.com>
2021-02-23 21:36:47 -0600

    cleaned up code a little

--- CS4323_OperatingSystems/Assignment01/process.c
+++ CS4323_OperatingSystems/Assignment01/process.c
@@ -5,9 +5,9 @@ void customerProcess(int letter, int msgID);
 int getRandom();
 void shuffle(int *arr, size_t n);
 
-char * receiveMessage(mqd_t msgID);
+char *receiveMessage(mqd_t msgID);
 
-#define QUEUE_NAME   "/addition"
+#define QUEUE_NAME "/addition"
 #define PERMISSIONS 0660
 #define MAX_MESSAGES 10
 #define MAX_MSG_SIZE 1024
@@ -15,21 +15,21 @@ char * receiveMessage(mqd_t msgID);
 
 void test(struct Item *perm)
 {
-    int *count = mmap (NULL, sizeof (int),
-    PROT_READ | PROT_WRITE,
-    MAP_SHARED, -1, 0);
+    int *count = mmap(NULL, sizeof(int),
+                      PROT_READ | PROT_WRITE,
+                      MAP_SHARED, -1, 0);
 
-    mqd_t queueDescriptor;   // queue descriptors
+    mqd_t queueDescriptor; // queue descriptors
 
-	struct mq_attr attr;
-	attr.mq_flags =IPC_NOWAIT;
-	attr.mq_maxmsg = MAX_MESSAGES;	// The maximum number of messages that can be stored on the queue. 
-	attr.mq_msgsize = MAX_MSG_SIZE;	// The maximum size of each message on the given message queue. 
-	attr.mq_curmsgs = 0;	// This field represents the number of messages currently on the given queue.
+    struct mq_attr attr;
+    attr.mq_flags = IPC_NOWAIT;
+    attr.mq_maxmsg = MAX_MESSAGES;  // The maximum number of messages that can be stored on the queue.
+    attr.mq_msgsize = MAX_MSG_SIZE; // The maximum size of each message on the given message queue.
+    attr.mq_curmsgs = 0;            // This field represents the number of messages currently on the given queue.
 
-    if ((queueDescriptor = mq_open (QUEUE_NAME, O_RDWR | O_CREAT, PERMISSIONS, &attr)) == -1) {
-        perror ("Child: mq_open");
-        exit (1);
+    if ((queueDescriptor = mq_open(QUEUE_NAME, O_RDWR | O_CREAT, PERMISSIONS, &attr)) == -1){
+        perror("Child: mq_open");
+        exit(1);
     }
 
     int numItems;
@@ -46,24 +46,28 @@ void test(struct Item *perm)
     pid_t pid = fork();
 
     // "Helper" process will execute
-    if (pid == 0){helperProcess(queueDescriptor, attr, numCustomers, order, perm);}
+    if (pid == 0)
+    {
+        helperProcess(queueDescriptor, attr, numCustomers, order, perm);
+    }
 
-    else if (pid > 0){
-
+    else if (pid > 0)
+    {
         int i = 0;
 
         while (i < numCustomers && pid > 0)
         {
             pid = fork();
-            if(pid > 0) wait(NULL);
+            if (pid > 0)
+                wait(NULL);
             letter++;
             i++;
         }
     }
 
     // "Customer" processes will execute
-    if (pid == 0){
-
+    if (pid == 0)
+    {
         int priority;
         for (int i = 0; i < numCustomers; i++)
         {
@@ -80,36 +84,34 @@ void test(struct Item *perm)
 
         itemsToGet[0] = numItems;
 
-        printf("Customer %c has %d items\n", letter, itemsToGet[0]);
-        for (i = 0; i < numItems; i++){
-
+        printf("Cust:(%c) (%d) has %d items -->", letter, getpid(), itemsToGet[0]);
+        for (i = 0; i < numItems; i++)
+        {
             randomItem = getRandom(getpid() + i);
             itemsToGet[i + 1] = randomItem;
             printf("%d ", itemsToGet[i + 1]);
         }
 
-        printf("\n");
+        if ((queueDescriptor = mq_open(QUEUE_NAME, O_WRONLY | O_CREAT, PERMISSIONS, &attr)) == -1){
+            perror("Child: mq_open");
+            exit(1);
+        }
 
-		if ((queueDescriptor = mq_open (QUEUE_NAME, O_WRONLY | O_CREAT, PERMISSIONS, &attr)) == -1) {
-			perror ("Child: mq_open");
-			exit (1);
-		}
-
-		char out_buffer [numItems];
+        char out_buffer[numItems];
 
         for (int i = 0; i < numItems; i++)
         {
             out_buffer[i] = itemsToGet[i];
         }
 
-        if (mq_send (queueDescriptor, out_buffer, strlen (out_buffer) + 1, priority) == -1) {
-            perror ("Child: Not able to send message to the parent process..");
+        if (mq_send(queueDescriptor, out_buffer, strlen(out_buffer) + 1, priority) == -1){
+            perror("Child: Not able to send message to the parent process..");
             exit(1);
         }
         printf("Sent!\n");
+        printf("---------------------------------------------\n");
     }
 }
-
 
 void helperProcess(mqd_t queueDescriptor, struct mq_attr attr, int numCustomers, char order[], struct Item *perm)
 {
@@ -146,41 +148,42 @@ void helperProcess(mqd_t queueDescriptor, struct mq_attr attr, int numCustomers,
     printf("");
     fclose(f);
 
-    if (mq_close (queueDescriptor) == -1) {
-        perror ("Parent: mq_close");
-        exit (1);
+    if (mq_close(queueDescriptor) == -1){
+        perror("Parent: mq_close");
+        exit(1);
     }
 
-    if (mq_unlink (QUEUE_NAME) == -1) {
-        perror ("Parent: mq_unlink");
-        exit (1);
+    if (mq_unlink(QUEUE_NAME) == -1){
+        perror("Parent: mq_unlink");
+        exit(1);
     }
-            
+
     exit(0);
 }
 
-char * receiveMessage(mqd_t queueDescriptor){
+char *receiveMessage(mqd_t queueDescriptor)
+{
+    char in_buffer[MSG_BUFFER_SIZE];
 
-    char in_buffer [MSG_BUFFER_SIZE];
-
-    if (mq_receive (queueDescriptor, in_buffer, MSG_BUFFER_SIZE, 0) == -1) {
-        perror ("Parent: mq_receive");
-        exit (1);
+    if (mq_receive(queueDescriptor, in_buffer, MSG_BUFFER_SIZE, 0) == -1)
+    {
+        perror("Parent: mq_receive");
+        exit(1);
     }
 
     int numItems = in_buffer[0];
 
     char *s = malloc(sizeof(char) * numItems);
 
-    for(int i = 0; i < numItems; i++)
+    for (int i = 0; i < numItems; i++)
     {
         s[i] = in_buffer[i];
     }
     return s;
 }
 
-int getRandom(int pid){
-
+int getRandom(int pid)
+{
     int lower = 1, upper = 99;
 
     srand(pid * time(0));
@@ -206,7 +209,6 @@ int getRandom(int pid){
 //     fclose(f);
 // }
 
-
 //     int zeroToHund[100];
 
 // for (i = 0; i < 100; i++){
@@ -221,11 +223,11 @@ int getRandom(int pid){
 
 //     void shuffle(int *arr, size_t n)
 // {
-//     if (n > 1) 
+//     if (n > 1)
 //     {
 //         size_t i;
 //         srand(time(NULL));
-//         for (i = 0; i < n - 1; i++) 
+//         for (i = 0; i < n - 1; i++)
 //         {
 //           size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
 //           int t = arr[j];
