2170b9c0514c1b684fad74c82fe7acec35ea706c
Parker Hague <parkerhague@gmail.com>
2021-03-05 00:38:24 -0600

    Tests sending Query struct and searching history

--- GroupAssignment1/header.h
+++ GroupAssignment1/header.h
@@ -1,8 +1,15 @@
 
-#include <stdio.h>     // Baisc standard I/O like printf
-#include <string.h>    // required for strlen
-#include <unistd.h>    // defines standard symbolic constants and types
+#include <stdlib.h> // needed for file operations
+#include <stdio.h> // Baisc standard I/O like printf
+#include <string.h> // required for strlen
+#include <unistd.h> // defines standard symbolic constants and types
 #include <sys/types.h> // basic derived types like pid_t
-#include <sys/stat.h>  //required for mkfifo (i.e. defines data returned by the stat() function: get file status)
-#include <fcntl.h>     //required for open (i.e. file control options)
+#include <sys/stat.h> //required for mkfifo (i.e. defines data returned by the stat() function: get file status)
+#include <fcntl.h> //required for open (i.e. file control options)
 
+struct Query
+{
+    char employeeName[30];
+    char jobTitle[128];
+    char status[3];
+};
--- GroupAssignment1/parker_PipeSendTest.c
+++ GroupAssignment1/parker_PipeSendTest.c
@@ -1,38 +1,57 @@
 #include "header.h"
 
-struct Test
-{
-    int age;
-    char name[30];
-};
 
-void pipeSend()
+void pipeSend(char *employeeName, char *jobTitle, char *status)
 {
     int fd; // file descriptor
 
-    struct Test test;
-    struct Test *testPtr = &test;
+    struct Query send;
+    struct Query *sendPtr = &send;
 
-    testPtr->age = 22;
-    strcpy(testPtr->name, "Parker Hague");
+    strcpy(sendPtr->employeeName, employeeName);
+    strcpy(sendPtr->jobTitle, jobTitle);
+    strcpy(sendPtr->status, status);
 
-    printf("age: %d\n", testPtr -> age);
-    printf("name: %s\n", testPtr -> name );
+    printf("Employee Name: %s\n Job Title: %s\n Status: %s\n", sendPtr->employeeName, sendPtr->jobTitle, sendPtr->status); // Print the read message
 
     char myPipe[] = "./NamedPipe"; // FIFO file path
     mkfifo(myPipe, 0666);          // Creating the named file(FIFO)
 
     if ((fd = open(myPipe, O_WRONLY | O_CREAT)) < 0) // Open FIFO for write only
+    {
         printf("1: Error opening..");
+    }
 
-    if (write(fd, testPtr, sizeof(test) * 2) < 0) // Write on the FIFO
+    if (write(fd, sendPtr, sizeof(struct Query)) < 0) // Write on the FIFO
+    {
         printf("1: Error writing..");
+    }
 
     close(fd); // Close the pipe
 }
 
 int main()
 {
-    pipeSend();
+    //EmployeeName, JobTitle and Status
+    // NATHANIEL FORD GENERAL MANAGER-METROPOLITAN TRANSIT AUTHORITY PT
+    // CAPTAIN III (POLICE DEPARTMENT) FT
+    // GARY JIMENEZ CAPTAIN III (POLICE DEPARTMENT) FT
+
+    char employeeName[3][30];
+    strcpy(employeeName[0], "NATHANIEL FORD");
+    strcpy(employeeName[1], "GARY JIMENEZ");
+    strcpy(employeeName[2], "ALBERT PARDINI");
+
+    char jobTitle[3][128];
+    strcpy(jobTitle[0], "GENERAL MANAGER-METROPOLITAN TRANSIT AUTHORITY");
+    strcpy(jobTitle[1], "CAPTAIN III (POLICE DEPARTMENT)");
+    strcpy(jobTitle[2], "CAPTAIN III (POLICE DEPARTMENT)");
+
+    char status[3][3];
+    strcpy(status[0], "PT");
+    strcpy(status[1], "FT");
+    strcpy(status[2], "Ft");
+
+    pipeSend(employeeName[1], jobTitle[1], status[1]);
     return 0;
 }
