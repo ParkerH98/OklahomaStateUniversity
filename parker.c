ad48d3d3c472d823da30eb43101246db08221b7d
Parker Hague <parkerhague@gmail.com>
2021-03-28 21:23:19 -0500

    -m

--- parker.c
+++ parker.c
@@ -187,38 +187,38 @@ void serverSocket_SendReceive()
     if (listen(entrySocket, 5) == 0) { printf("SERVER: Listening....\n"); }
     else { printf("[-]Error in listening"); }
 
-    for (int i = 0; i < TESTING_LOOP; i++)
-    {
-        // Accept call creates a new socket for the incoming connection
-        addr_size = sizeof serverStorage;
-        connectionSocket = accept(entrySocket, (struct sockaddr *)&serverStorage, &addr_size);
+    // for (int i = 0; i < TESTING_LOOP; i++)
+    // {
+    //     // Accept call creates a new socket for the incoming connection
+    //     addr_size = sizeof serverStorage;
+    //     connectionSocket = accept(entrySocket, (struct sockaddr *)&serverStorage, &addr_size);
 
-        struct Query query;
-        struct Query *queryPtr = &query;
+    //     struct Query query;
+    //     struct Query *queryPtr = &query;
 
-        recv(connectionSocket, queryPtr, sizeof(struct Query), 0);                                                                      //Read the message from the server into the buffer
-        printf("SERVER: Query received from assistant:\n\n%s\n%s\n%s\n", queryPtr->employeeName, queryPtr->jobTitle, queryPtr->status); //Print the received message
+    //     recv(connectionSocket, queryPtr, sizeof(struct Query), 0);                                                                      //Read the message from the server into the buffer
+    //     printf("SERVER: Query received from assistant:\n\n%s\n%s\n%s\n", queryPtr->employeeName, queryPtr->jobTitle, queryPtr->status); //Print the received message
 
-        struct EmployeeStructure employee;
-        struct EmployeeStructure *employeePtr = &employee;
+    //     struct EmployeeStructure employee;
+    //     struct EmployeeStructure *employeePtr = &employee;
 
-        employeePtr->id = 15000;
-        strcpy(employeePtr->employeeName, "BRIAN BENSON");
-        strcpy(employeePtr->jobTitle, "IS BUSINESS ANALYST");
-        employeePtr->basePay = 78059.8;
-        employeePtr->overtimePay = 0;
-        employeePtr->benefit = 0;
-        strcpy(employeePtr->status, "FT");
-        employeePtr->satisfactionLevel = 0.37;
-        employeePtr->numberProject = 2;
-        employeePtr->averageMonthlyHours = 158;
-        employeePtr->yearsInCompany = 3;
-        employeePtr->workAccident = 0;
-        employeePtr->promotionsLast5Years = 0;
+    //     employeePtr->id = 15000;
+    //     strcpy(employeePtr->employeeName, "BRIAN BENSON");
+    //     strcpy(employeePtr->jobTitle, "IS BUSINESS ANALYST");
+    //     employeePtr->basePay = 78059.8;
+    //     employeePtr->overtimePay = 0;
+    //     employeePtr->benefit = 0;
+    //     strcpy(employeePtr->status, "FT");
+    //     employeePtr->satisfactionLevel = 0.37;
+    //     employeePtr->numberProject = 2;
+    //     employeePtr->averageMonthlyHours = 158;
+    //     employeePtr->yearsInCompany = 3;
+    //     employeePtr->workAccident = 0;
+    //     employeePtr->promotionsLast5Years = 0;
 
-        send(connectionSocket, employeePtr, sizeof(struct EmployeeStructure), 0);
-        printf("\nSERVER: Result sent to assistant.\n\n");
-    }
+    //     send(connectionSocket, employeePtr, sizeof(struct EmployeeStructure), 0);
+    //     printf("\nSERVER: Result sent to assistant.\n\n");
+    // }
 }
 /*
 ---------------------------------------------------------
@@ -389,7 +389,7 @@ void assistant()
     
     if (searchFile(fname, query.employeeName, query.jobTitle, query.status,&employee) != 0) // a match was found
     {
-        printToTerminal(employee);
+        // printToTerminal(employee);
         sleep(1);
     }
     else // a match wasn't found
@@ -399,7 +399,7 @@ void assistant()
             writeFile(fname, employee, writepos); // writes resulted employee to history file
             writepos = (writepos + 1) % HISTORYMAX;
 
-            printToTerminal(employee); // prints the resulted employee information to a new terminal
+            // printToTerminal(employee); // prints the resulted employee information to a new terminal
             sleep(1);
 
         }
