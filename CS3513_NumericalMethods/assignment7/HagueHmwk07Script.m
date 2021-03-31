63ff9f1385cffa85a448d8b98ac9c2d1d31ede7d
Parker Hague <parkerhague@gmail.com>
2021-03-30 23:08:11 -0500

    assn7

---
+++ CS3513_NumericalMethods/assignment7/HagueHmwk07Script.m
@@ -0,0 +1,34 @@
+%Part a:
+
+% given
+A = [4^2 4 1;
+     2^2 2 1;
+     7^2 7 1];
+ 
+ % Row-Sum Norm
+ infNorm = norm(A, inf);
+ infCond = cond(A, inf);
+ 
+ % Part b:
+ 
+ % Spectral
+ spNorm = norm(A, 2);
+ spCond = cond(A, 2);
+
+ % Frobenius
+ froNorm = norm(A, 'fro');
+ froCond = cond(A, 'fro');
+ 
+ % create strings
+ infString = sprintf( 'Infinity Condition: %f', infCond);
+ spString = sprintf( 'Spectral Condition: %f', spCond);
+ froString = sprintf( 'Frobenius Condition: %f', froCond);
+
+ % print answers
+ disp('Part a:')
+ disp(infString)
+ 
+ disp('Part b:')
+ disp(spString)
+ disp(froString)
+ 
