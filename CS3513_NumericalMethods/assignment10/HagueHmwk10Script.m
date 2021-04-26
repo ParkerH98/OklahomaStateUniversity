07818e99265b059acb2b952b480ca8b4701de78e
Parker Hague <parkerhague@gmail.com>
2021-04-26 15:48:00 -0500

    -m

--- CS3513_NumericalMethods/assignment10/HagueHmwk10Script.m
+++ CS3513_NumericalMethods/assignment10/HagueHmwk10Script.m
@@ -3,10 +3,8 @@ X = [15 45 75 105 135 165 225 255 285 315 345];
 Y = [3.4 4.7 8.5 11.7 16 18.7 19.7 17.1 12.7 7.7 5.1];
 
 % gets amplitude, mean, and max
-[funcMean, funcAmplitude, funcMaxDay] = leastSquaresReg(X, Y);
+[funcMean, funcAmplitude, funcMaxDay, coeffs] = leastSquaresReg(X, Y, 5);
 
-% gets coefficients from regression
-coeffs = nonLinearReg(X, Y, 5);
 
 %answers
 disp("Mean");
--- CS3513_NumericalMethods/assignment10/leastSquaresReg.m
+++ CS3513_NumericalMethods/assignment10/leastSquaresReg.m
@@ -1,4 +1,4 @@
-function [funcMean, funcAmplitude, funcMaxDay] = leastSquaresReg(X, Y)
+function [funcMean, funcAmplitude, funcMaxDay, coeffs] = leastSquaresReg(X, Y, order)
 % input: X is a vector of x values 
 % input: Y is a vector of y values
 
@@ -37,6 +37,20 @@ func(i) = slope * i + intercept; % function value at each day of the year
 sumFunc = sumFunc + func(i); % summation of func
 
 end
+rows = length(X); % rows of matrix
+
+X = X'; % transposes the x vector
+
+M = ones(rows, order + 1); % makes a working matrix of size x by order + 1
+
+for i = order : -1 : 1 % decrements from order to -1
+    
+    % goes through each column from right to left multiplying 
+    % each x value by a power of n where n is from 0 to order
+    M(:, i) = M(:, i + 1) .* X;
+end
+coeffs = (M \ Y'); % inv(M) * yVect' to compute p vector of coefficients if size m + 1
+% gets coefficients from regression
 
 funcMean = sumFunc/period; % sum of func / the period to obtain the mean
 
@@ -54,3 +68,4 @@ funcAmplitude = max(diff); % the max difference is the amplitude for the functio
 [temperature, funcMaxDay] = max(func); % gets the day with the max temperature
 funcMaxDay = funcMaxDay - adj;
 end
+
