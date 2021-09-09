e84d773e678541259bd334be93d2e89f38d93da8
Parker Hague <parkerhague@gmail.com>
2021-09-09 18:57:03 -0500

    finished assignment2

---
+++ CS4173_VideoGameDevelopment/CCUnit2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogSpawnInterval = 0.7f;
+    private float lastSpawnTime;
+
+
+    void Start()
+    {
+        lastSpawnTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && (Time.time - lastSpawnTime > dogSpawnInterval))
+        {
+            lastSpawnTime = Time.time;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ CS4173_VideoGameDevelopment/CCUnit2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnRandomBall", spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        int r = Random.Range(0, 3);
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[r], spawnPos, ballPrefabs[r].transform.rotation);
+        spawnInterval = Random.Range(3, 6);
+        print(spawnInterval);
+        Invoke("SpawnRandomBall", spawnInterval);
+    }
+}
