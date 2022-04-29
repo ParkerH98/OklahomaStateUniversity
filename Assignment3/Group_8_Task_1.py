7b618ef6bc89df697d71ea034e2b5202530f3048
Dax Jones <48337068+daxljones@users.noreply.github.com>
2022-04-29 01:04:43 -0500

    Starting to finish project

---
+++ Assignment3/Group_8_Task_1.py
@@ -0,0 +1,88 @@
+from matplotlib.transforms import Transform
+
+
+import pyspark.sql.functions as F
+from pyspark.ml import Pipeline, Transformer
+from pyspark.ml.feature import Bucketizer
+from pyspark.sql import DataFrame
+from typing import Iterable
+import pandas as pd
+
+class DataDoctorer(Transformer):
+    
+    def __init__(self, spark):
+        super(DataDoctorer, self).__init__()
+        self.spark = spark
+        
+
+    def _transform(self, df: DataFrame) -> DataFrame:
+        df = self.rm_out_of_range_all(df)
+        df = self.remove_NULL_values(df) 
+        return df
+
+    
+    
+    def df_spark_to_pandas(self, spark_df):
+      pandas_df = spark_df.select("*").toPandas()
+      return pandas_df
+
+
+
+
+    def apply_knn_imputer(pandas_df):
+        imputer = KNNImputer(n_neighbors=10)
+        knn_imputed_matrix = imputer.fit_transform(pandas_df)
+        corrected_df = pd.DataFrame(knn_imputed_matrix, columns=[
+            "housing_median_age",
+            "total_rooms",
+            "total_bedrooms",
+            "population",
+            "median_house_value",
+            "ocean_proximity_index"])
+        return corrected_df
+
+
+
+
+
+
+    def remove_NULL_values(self, house_price_df):
+        pandas_df = self.df_spark_to_pandas(house_price_df)
+        pandas_df = self.apply_knn_imputer(pandas_df)
+        house_price_df = self.spark.createDataFrame(pandas_df)
+
+        return house_price_df
+
+
+
+
+
+
+    def rm_oor_operations(self, house_price_df, col_to_correct, lower_bound, upper_bound):
+        sdf = self.df_spark_to_pandas(house_price_df)
+
+        out_of_range = house_price_df.filter(
+            ~col(col_to_correct).between(lower_bound, sdf[col_to_correct].mean() + upper_bound))
+        out_of_range.show()
+
+        house_price_df = house_price_df.withColumn(col_to_correct, when(
+            ~col(col_to_correct).between(lower_bound, sdf[col_to_correct].mean() + upper_bound), None).otherwise(col(col_to_correct)))
+        return house_price_df
+
+
+
+
+
+
+    def rm_out_of_range_all(self, house_price_df):
+        sdf = self.df_spark_to_pandas(house_price_df)
+
+        house_price_df = self.rm_oor_operations(
+            house_price_df, "housing_median_age", 0, 2 * sdf['housing_median_age'].std())
+        house_price_df = self.rm_oor_operations(
+            house_price_df, "total_rooms", 0, 4 * sdf['total_rooms'].std())
+        house_price_df = self.rm_oor_operations(
+            house_price_df, "total_bedrooms", 0, 4 * sdf['total_bedrooms'].std())
+        house_price_df = self.rm_oor_operations(
+            house_price_df, "population", 0, 4 * sdf['population'].std())
+        return house_price_df
--- Assignment3/Group_8_Task_2b.py
+++ Assignment3/Group_8_Task_2b.py
@@ -3,61 +3,84 @@ from pyspark.ml.regression import LinearRegression
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.feature import StringIndexer
+from pyspark.ml.feature import OneHotEncoder
 from pyspark.ml.evaluation import RegressionEvaluator
-
 from pyspark.ml import Pipeline
+import pandas as pd
+from sklearn.impute import KNNImputer
+from pyspark.sql.functions import col, isnan, when, count
+from Group_8_Task_1 import DataDoctorer
 
-spark = SparkSession.builder.getOrCreate()
-spark.sparkContext.setLogLevel('WARN')
+
+def main():
+    spark = SparkSession.builder.getOrCreate()
+    spark.sparkContext.setLogLevel('OFF')
+
+    print("\n\n\n\n\nStarting process.\n")
+        
+    # reads dataset from file in HDFS to a spark dataframe
+    house_price_df = spark\
+        .read\
+        .option("inferSchema", "true")\
+        .option("header", "true")\
+        .csv("hdfs:///user/kaggle/kaggle_data/california_housing.csv")
 
     
-# reads dataset from file in HDFS to a spark dataframe
-house_price_df = spark\
-    .read\
-    .option("inferSchema", "true")\
-    .option("header", "true")\
-    .csv("hdfs:///user/kaggle/kaggle_data/california_housing.csv")
-
-indexer = StringIndexer(inputCol="ocean_proximity",
-                        outputCol="ocean_proximity_index")
-# house_price_df = indexer.fit(house_price_df).transform(house_price_df)
-
-# Split data into training/testing
-split_data = house_price_df.randomSplit([.75, .25], 100)
-training_data = split_data[0]
-test_data = split_data[1]
-
-
-# Build vector assembler
-vectorAssembler = VectorAssembler(inputCols=['longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms',
-                                  'population', 'households', 'median_income', 'ocean_proximity_index'], outputCol='features')
-
-# usable_df = vectorAssembler.transform(house_price_df)                 May be used later but pipeline should do it
-# usable_df = usable_df.select(['features', 'median_house_value'])
-
-
-# Build Model
-lr_model = LinearRegression(featuresCol='features', labelCol='median_house_value')
-rf_model = RandomForestRegressor(featuresCol="features", labelCol='median_house_value', predictionCol="predictions", maxBins=50, seed=100)
-
-
-# Build Evaluator
-evaluator = RegressionEvaluator(labelCol="median_house_value",
-                                predictionCol="predictions",
-                                metricName="rmse")
-
-# Create pipelines
-lr_pipeline = Pipeline(stages=[indexer, vectorAssembler, lr_model, evaluator])
-rf_pipeline = Pipeline(stages=[indexer, vectorAssembler, rf_model, evaluator])
-
-lr_pipeline_model = lr_pipeline.fit(training_data)
-rf_pipeline_model = rf_pipeline.fit(training_data)
-
-lr_results = lr_pipeline_model.transform(test_data)
-rf_results = lr_pipeline_model.transform(test_data)
-
-lr_results.show()
-rf_results.show()
+    cols_to_remove = ("longitude", "latitude", "households", "median_income")
+    house_price_df = house_price_df.drop(*cols_to_remove)
 
 
 
+    # Split data into training/testing
+    split_data = house_price_df.randomSplit([.75, .25], 100)
+    training_data = split_data[0]
+    test_data = split_data[1]
+
+
+    # Define Indexer
+    indexer = StringIndexer(inputCol="ocean_proximity",
+                            outputCol="ocean_proximity_index")
+
+    data_doctorer = DataDoctorer(spark)   
+
+    # Build vector assembler
+    vectorAssembler = VectorAssembler(inputCols=['housing_median_age', 'total_rooms', 'total_bedrooms',
+                                    'population', 'ocean_proximity_index'], outputCol='features')
+
+
+    # Build Model
+    lr_model = LinearRegression(featuresCol='features', labelCol='median_house_value', predictionCol="predicions")
+    rf_model = RandomForestRegressor(featuresCol="features", labelCol='median_house_value', predictionCol="predictions", maxBins=50, seed=100)
+
+
+    # Build Evaluator
+    evaluator = RegressionEvaluator(labelCol="median_house_value",
+                                    predictionCol="predictions",
+                                    metricName="rmse")
+
+    # Create pipelines
+    lr_pipeline = Pipeline(stages=[indexer, data_doctorer, vectorAssembler, lr_model])
+    rf_pipeline = Pipeline(stages=[indexer, data_doctorer, vectorAssembler, rf_model])
+    
+    print(">Fitting Pipelines\n")
+
+    # Fit the models
+    lr_pipeline_model = lr_pipeline.fit(training_data)
+    rf_pipeline_model = rf_pipeline.fit(training_data)
+
+
+    print(">Making Tests\n")
+    # Make predictions 
+    lr_results = lr_pipeline_model.transform(test_data)
+    rf_results = rf_pipeline_model.transform(test_data)
+
+    print("Result of Linear Regression\n")
+    lr_results.show(10)
+    print("\nResult of Linear Regression\n")
+    rf_results.show(10)
+    print("\n\nRMSE for Linear Regression: ", evaluator.evaluate(lr_results))
+    print("\nRMSE for Random Forrest: ", evaluator.evaluate(rf_results))
+
+
+if __name__ == "__main__":
+    main()
