4f91ccdc1ed27f4c2d8621ab819c3bfb5fa50820
Parker Hague <parkerhague@gmail.com>
2022-04-29 22:19:05 -0500

    ran formatter

--- Assignment3/Group_8_Task_1.py
+++ Assignment3/Group_8_Task_1.py
@@ -7,28 +7,22 @@ from pyspark.sql.functions import col, isnan, when, count
 
 
 class DataDoctorer(Transformer):
-    
+
     def __init__(self, spark):
         super(DataDoctorer, self).__init__()
         self.spark = spark
-        
 
     def _transform(self, df: DataFrame) -> DataFrame:
         df = self.rm_out_of_range_all(df)
-        df = self.remove_NULL_values(df) 
+        df = self.remove_NULL_values(df)
         return df
 
-    
-    
     def df_spark_to_pandas(self, spark_df):
-      pandas_df = spark_df.select("*").toPandas()
-      return pandas_df
-
-
-
+        pandas_df = spark_df.select("*").toPandas()
+        return pandas_df
 
     def apply_knn_imputer(self, pandas_df):
-        imputer = KNNImputer(n_neighbors=10)
+        imputer = KNNImputer(n_neighbors=4)
         knn_imputed_matrix = imputer.fit_transform(pandas_df)
         corrected_df = pd.DataFrame(knn_imputed_matrix, columns=[
             "housing_median_age",
@@ -39,22 +33,12 @@ class DataDoctorer(Transformer):
             "ocean_proximity_index"])
         return corrected_df
 
-
-
-
-
-
     def remove_NULL_values(self, house_price_df):
         pandas_df = self.df_spark_to_pandas(house_price_df)
         pandas_df = self.apply_knn_imputer(pandas_df)
         house_price_df = self.spark.createDataFrame(pandas_df)
 
         return house_price_df
-
-
-
-
-
 
     def rm_oor_operations(self, house_price_df, col_to_correct, lower_bound, upper_bound):
         sdf = self.df_spark_to_pandas(house_price_df)
@@ -65,11 +49,6 @@ class DataDoctorer(Transformer):
         house_price_df = house_price_df.withColumn(col_to_correct, when(
             ~col(col_to_correct).between(lower_bound, sdf[col_to_correct].mean() + upper_bound), None).otherwise(col(col_to_correct)))
         return house_price_df
-
-
-
-
-
 
     def rm_out_of_range_all(self, house_price_df):
         sdf = self.df_spark_to_pandas(house_price_df)
